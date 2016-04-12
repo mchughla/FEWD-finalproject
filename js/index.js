3e3eb65cb0bd5ab10ea2b41a8bbe89a6ccf207c8
Laura <lauramchugh@Lauras-MacBook-Air.local>
2016-04-12 08:28:45 -0400

    final

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+// var now = new Date();
+// var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+// var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+// function fourdigits(number) {
+//     return (number < 1000) ? number + 1900 : number;
+// }
+// today =  months[now.getMonth()] + " " +
+//          date + ", " +
+//          (fourdigits(now.getYear())) ;
+// document.write(today);
+
+
+
+	});
