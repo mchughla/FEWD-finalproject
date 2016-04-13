a0e4745919a13701314ed9af105dbac92e195b1d
Laura <Laura@Mac-Pro.local>
2016-04-13 13:57:50 -0400

    4-13

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+    
+//create function for navigation clicks to scroll 
 $(document).ready(function() {
+    $(".scroll").click(function(event) {
+    event.preventDefault();
+    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
+    } );
+  });
 
 //Create array to hold names of work photos 
+$(document).ready(function() {
+	 event.preventDefault();
 	var work = [
 		'work1',
 		'work2',
@@ -11,8 +20,7 @@ $(document).ready(function() {
 	];
 
 function createWorkHTML (workPhoto) {
-		var imageHTML = '<img id="carousel" src="images/' + workPhoto.toLowerCase() + '.png" />';
-
+		var imageHTML = '<img id="work" src="images/work/' + workPhoto.toLowerCase() + '.png" />';
 		return imageHTML;
 	}
 
@@ -54,6 +62,124 @@ function createWorkHTML (workPhoto) {
 		var nextWorkImageHTML = workImageHTML[carouselIndex];
 
 		$('#carousel-view').html(nextWorkImageHTML);
+	}
+
+	});
+
+//Create array to hold names of travel photos 
+$(document).ready(function() {
+	 event.preventDefault();
+	var travel = [
+		'travel1',
+		'travel3',
+		'travel4',
+		'travel5',
+		'travel6'
+	];
+
+function createTravelHTML (travelPhoto) {
+		var imageHTML = '<img id="travel" src="images/travel/' + travelPhoto.toLowerCase() + '.jpg" />';
+
+		return imageHTML;
+	}
+
+	var travelImageHTML = [];
+
+	for (var i = 0; i < travel.length; i++) {
+		var travelPhoto = travel[i];
+		var html = createTravelHTML(travelPhoto);
+
+		travelImageHTML.push(html);
+	}
+
+	var carouselIndex = 0;
+
+	$('#next-travel').click(showNextImage);
+	$('#previous-travel').click(showPreviousImage);
+
+
+	function showNextImage() {
+		carouselIndex++;
+
+		if (carouselIndex > travelImageHTML.length - 1) {
+			carouselIndex = 0;
+		}
+
+		var nextTravelImageHTML = travelImageHTML[carouselIndex];
+
+		$('#carousel-view-travel').html(nextTravelImageHTML);
+		$('nextTravelImageHTML').slide({left: 0});
+	}
+
+	function showPreviousImage() {
+		carouselIndex--;
+
+		if (carouselIndex < 0) {
+			carouselIndex = travelImageHTML.length - 1;
+		}
+
+		var nextTravelImageHTML = travelImageHTML[carouselIndex];
+
+		$('#carousel-view-travel').html(nextTravelImageHTML);
+	}
+
+	});
+
+	//Create array to hold names of food photos 
+$(document).ready(function() {
+	 event.preventDefault();
+	var food = [
+		'food1',
+		'food2',
+		'food3',
+		'food4',
+		'food5'
+	];
+
+function createFoodHTML (foodPhoto) {
+		var imageHTML = '<img id="food" src="images/food/' + foodPhoto.toLowerCase() + '.jpg" />';
+
+		return imageHTML;
+	}
+
+	var foodImageHTML = [];
+
+	for (var i = 0; i < food.length; i++) {
+		var foodPhoto = food[i];
+		var html = createFoodHTML(foodPhoto);
+
+		foodImageHTML.push(html);
+	}
+
+	var carouselIndex = 0;
+
+	$('#next-food').click(showNextImage);
+	$('#previous-food').click(showPreviousImage);
+
+
+	function showNextImage() {
+		carouselIndex++;
+
+		if (carouselIndex > foodImageHTML.length - 1) {
+			carouselIndex = 0;
+		}
+
+		var nextFoodImageHTML = foodImageHTML[carouselIndex];
+
+		$('#carousel-view-food').html(nextFoodImageHTML);
+		$('nextFoodImageHTML').slide({left: 0});
+	}
+
+	function showPreviousImage() {
+		carouselIndex--;
+
+		if (carouselIndex < 0) {
+			carouselIndex = foodImageHTML.length - 1;
+		}
+
+		var nextFoodImageHTML = foodImageHTML[carouselIndex];
+
+		$('#carousel-view-food').html(nextFoodImageHTML);
 	}
 
 	});
