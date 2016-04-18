89261b18aae3481d4d988a36bfc66bbe9583e1f4
Laura <Laura@Mac-Pro.local>
2016-04-18 17:55:36 -0400

    final edits

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function scrollNav() {
   $('.nav a').click(function(){  
     //Animate
     $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
+        scrollTop: $( $(this).attr('href') ).offset().top - 50
     }, 400);
     return false;
   });
